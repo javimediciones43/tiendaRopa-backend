4d098136b352d780418484750749f0d3f6123079
javimediciones <javiero_212@hotmail.com>
2025-07-29 21:45:03 -0300

    Create the roles and roles_user tables with their respective migration files.

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,6 +33,16 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function hasRole($role)
+    {
+        return $this->roles()->where('name', $role)->exists();
+    }
+
     /**
      * Get the attributes that should be cast.
      *
